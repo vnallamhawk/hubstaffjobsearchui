1b2843c806e30fe6975741445defbdccab74011a
Venkata Rakesh Nallam <vnallam@hawk.iit.edu>
2018-07-27 00:31:21 -0700

    chore():updating api

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,4 +1,4 @@
 const hostName = window&&window.location&&window.location.hostname;
-//var backEndHost = "https://hubstaffapi.herokuapp.com"
-var backEndHost = "http://localhost:3100"
+var backEndHost = "https://hubstaffapi.herokuapp.com"
+//var backEndHost = "http://localhost:3100"
 export const root = `${backEndHost}`
