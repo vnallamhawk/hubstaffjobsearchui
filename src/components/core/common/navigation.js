1063d8c580ddc4c2f4494cc9cc1a7405fbeee77b
Venkata Rakesh Nallam <vnallam@hawk.iit.edu>
2018-08-25 20:44:09 -0700

    style(): updating nav bar

--- src/components/core/common/navigation.js
+++ src/components/core/common/navigation.js
@@ -3,20 +3,23 @@ import { Icon } from 'antd';
 export const Navigation = () => {
     return (
         <div>
-            <header className="header-container">
- <div className="logo">
-   <a href="index.html" title="Hubstaff Talent">
-   <img src="images/logo.png" width="180"/>
-   </a>
- </div>
-    <nav>
-        <ul>
-            <li><a title="Home">HOW IT WORKS</a></li>
-        <li><a title="Browse">BROWSE </a><Icon type="down" /> </li>
-    <li><a title="search">SEARCH</a></li >
-        <li><a title="myAccount">My Account</a><Icon type="down" /></li >
-   </ul >
- </nav >
-</header>
-</div>)
+            <div className="toolbar">
+            <nav className="toolbar_navigation">
+                <div className="logo">
+                    <a href="index.html" title="Hubstaff Talent">
+                        <img src="images/logo.png" width="180" />
+                    </a>
+                </div>
+                <div className="spacer" />
+                <div className="navbar_items">
+                        <ul>
+                            <li><a title="Home">HOW IT WORKS</a></li>
+                            <li><a title="Browse">BROWSE </a><Icon type="down" /> </li>
+                            <li><a title="search">SEARCH</a></li >
+                            <li><a title="myAccount">My Account</a><Icon type="down" /></li >
+                        </ul >
+                </div>
+                </nav >
+            </div>
+        </div>)
 }
